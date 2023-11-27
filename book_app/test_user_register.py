be97106e5716c17cd4a70b8cb577ecb3c11f3215
Oz <osvaldocanales4@gmail.com>
2023-11-26 17:38:40 -0700

    Trying to add webdriver for selenium

--- book_app/test_user_register.py
+++ book_app/test_user_register.py
@@ -1,4 +1,11 @@
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+class HostTest(LiveServerTestCase):
+    def testHomePage(self):
+        driver = webdriver.Chrome()
+
+        driver.get('http://127.0.0.1:8000/accounts/login/')
+        assert "Login Page Selenium!" in driver.title
